fd91dd0ede53c392cb91098201be7a8d199f4b93
wbxyy <792939277@qq.com>
2022-04-12 17:29:52 +0800

    feat(header and footer): template

--- src/components/ShopFooter/index.vue
+++ src/components/ShopFooter/index.vue
@@ -1,14 +1,90 @@
 <template>
-  <footer></footer>
+  <footer class="footer">
+    <div class="help w">
+      <div v-for="(help, index) in helps" :key="index">
+        <dl>
+          <dt>{{ help.title }}</dt>
+          <dd v-for="(item, index) in help.list" :key="index">
+            {{ item }}
+          </dd>
+        </dl>
+      </div>
+      <div>
+        <dl>
+          <dt>帮助中心</dt>
+          <dd><img src="./img/wx_cz.jpg" alt="" /></dd>
+        </dl>
+      </div>
+    </div>
+    <div class="links">
+      <ul>
+        <li v-for="(item, index) in links" :key="index">
+          <a href="#">{{ item }}</a>
+        </li>
+      </ul>
+      <div class="address">地址：{{ address }}</div>
+      <div class="beian">京ICP备19006430号</div>
+    </div>
+  </footer>
 </template>
 <script>
 import { defineComponent } from 'vue'
 export default defineComponent({
   data() {
     return {
-      shortcut: [{ title: '我的订单' }, { title: '我的购物车' }, { title: '我的尚品汇' }, { title: '尚品汇会员' }, { title: '企业采购' }, { title: '关注尚品汇' }, { title: '合作招商' }, { title: '商家后台' }],
+      helps: [
+        { title: '购物指南', list: ['购物流程', '会员介绍', '生活旅行/团购', '常见问题', '购物指南'] },
+        { title: '购物指南', list: ['购物流程', '会员介绍', '生活旅行/团购', '常见问题', '购物指南'] },
+        { title: '购物指南', list: ['购物流程', '会员介绍', '生活旅行/团购', '常见问题', '购物指南'] },
+        { title: '购物指南', list: ['购物流程', '会员介绍', '生活旅行/团购', '常见问题', '购物指南'] },
+        { title: '购物指南', list: ['购物流程', '会员介绍', '生活旅行/团购', '常见问题', '购物指南'] },
+      ],
+      links: ['关于我们', '联系我们', '商家入驻', '营销中心', '友情链接', '关于我们', '联系我们', '商家入驻', '营销中心', '友情链接'],
+      address: '北京市昌平区宏福科技园综合楼6层',
     }
   },
 })
 </script>
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.footer {
+  font-size: 12px;
+  padding-top: 30px;
+  background-color: #eaeaea;
+}
+
+.help {
+  display: flex;
+  justify-content: space-between;
+  dl dt {
+    font-size: 18px;
+    line-height: 1.5;
+    font-weight: 700;
+  }
+
+  dl dd {
+    font-size: 14px;
+    line-height: 1.35;
+  }
+}
+
+.links {
+  padding-top: 30px;
+  padding-bottom: 30px;
+  border-top: 1px solid #e3e3e3;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  & > * {
+    margin-top: 12px;
+  }
+  ul {
+    display: flex;
+  }
+  ul li a {
+    padding: 0 12px;
+    border-right: 1px solid #b0b0b0;
+  }
+}
+</style>
--- src/components/ShopHeader/index.vue
+++ src/components/ShopHeader/index.vue
@@ -1,28 +1,31 @@
 <template>
-  <header>
+  <header class="header">
     <!-- 顶条 -->
     <div class="top_bar">
-      <div class="welcome">
-        <div class="title">尚硅谷欢迎您</div>
-        <div class="login">
-          <span>请登录</span>
-          |
-          <span>免费注册</span>
+      <div class="w">
+        <div class="welcome">
+          <div class="title">尚硅谷欢迎您！</div>
+          <div class="login">
+            <span>请登录</span>
+            |
+            <span>免费注册</span>
+          </div>
         </div>
-      </div>
-      <div class="shortcut">
-        <ul>
-          <li v-for="(item, index) in shortcut">
-            <a href="#">{{ item.title }}</a>
-          </li>
-        </ul>
+        <div class="shortcut">
+          <ul>
+            <li v-for="(item, index) in shortcut">
+              <a href="#">{{ item.title }}</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
     <!-- 底栏 -->
-    <div class="wrap">
+    <div class="wrap w">
       <div class="logo"><h1>尚品汇</h1></div>
       <div class="search">
-        <input type="search" />
+        <input class="search_ipt" type="text" />
+        <button class="search_btn">搜索</button>
       </div>
     </div>
   </header>
@@ -37,4 +40,80 @@ export default defineComponent({
   },
 })
 </script>
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.header {
+  font-size: 12px;
+  color: #676867;
+}
+.top_bar {
+  background-color: #eaeaea;
+  height: 30px;
+  .w {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .w .welcome,
+  .shortcut {
+    line-height: 30px;
+    height: 30px;
+    display: flex;
+  }
+
+  .w .welcome .title {
+    margin-right: 10px;
+  }
+
+  .w .shortcut ul {
+    display: flex;
+  }
+  .w .shortcut ul li a {
+    // display: inline-block;
+    padding: 0 12px;
+    border-right: 1px solid #b0b0b0;
+  }
+  .w .shortcut ul :last-child a {
+    border-right: none;
+  }
+}
+
+.wrap {
+  padding-top: 30px;
+  height: 240px;
+  display: flex;
+  justify-content: space-between;
+  .logo {
+    margin-left: 50px;
+
+    h1 {
+      margin: 0;
+      width: 176px;
+      height: 61px;
+      text-indent: -9999px;
+      background: url(./img/logo.png) no-repeat;
+    }
+  }
+  .search {
+    display: flex;
+    border: 1px solid #e35e40;
+    width: 702px;
+    height: 42px;
+    .search_ipt {
+      height: 40px;
+      width: 600px;
+      padding-left: 12px;
+      font-size: 16px;
+    }
+
+    .search_btn {
+      height: 40px;
+      line-height: 40px;
+      width: 100px;
+      background-color: #f75932;
+      color: #fff;
+      outline: none;
+    }
+  }
+}
+</style>
