71daaa35653b58118d0915fae11471791d3fb8d2
wbxyy <792939277@qq.com>
2022-04-13 10:30:23 +0800

    router

---
+++ src/router/index.js
@@ -0,0 +1,10 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+const routes = []
+
+const router = createRouter({
+  routes,
+  history: createWebHashHistory(),
+})
+
+export default router
